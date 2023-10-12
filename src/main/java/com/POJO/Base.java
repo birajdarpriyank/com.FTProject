ca4b85a0bf5d318cd60934492e333fa21a46ca59
Priyanka Birajdar <priyankabirajdar1118@gmail.com>
2023-10-12 21:55:44 +0530

    Add changes in Base class

--- src/main/java/com/POJO/Base.java
+++ src/main/java/com/POJO/Base.java
@@ -23,143 +23,14 @@ public class Base {
 	
 	public static WebDriver driver;
 	public Properties pro;
-	public WebDriverWait wait;
-	
-	public WebDriver Initialization(String url) throws Exception
+	 
+	public void BrowserInitialization()
 	{
-		
-		pro=new Properties();
-		FileInputStream file=new FileInputStream(".\\src\\test\\resources\\ConfigeProperties\\Confige.properties");
-		pro.load(file);  
-		
-		String Browsername = pro.getProperty("Browser");
-		if(Browsername.equalsIgnoreCase("chrome")) {
-		WebDriverManager.chromedriver().setup();
-		driver=new ChromeDriver();
-	}
-		else if(Browsername.equalsIgnoreCase("edge"))
-		{
-			WebDriverManager.edgedriver().setup();
-			driver = new EdgeDriver();
-		}
-		else if(Browsername.equalsIgnoreCase("Firefox"))
-		{
-			WebDriverManager.firefoxdriver().setup();
-			driver = new FirefoxDriver();
-		}
-		else if(Browsername.equalsIgnoreCase("safari"))
-		{
-			WebDriverManager.safaridriver().setup();
-			driver = new SafariDriver();
-		}
-		System.out.println(pro.getProperty("Browser"));
-		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		driver = new ChromeDriver();
+		pro = new Properties();
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
-		driver.get(pro.getProperty(url));
-		return driver;
-		
-	}
-	
-	public void waituntilVisibilityofElement(WebElement e)
-	{
-		wait = new WebDriverWait(driver,Duration.ofSeconds(10));
-		wait.until(ExpectedConditions.visibilityOf(e));
-	}
-	
-	public void waitelementtobeClikable(WebElement e)
-	{
-		wait = new WebDriverWait(driver,Duration.ofSeconds(10));
-		wait.until(ExpectedConditions.elementToBeClickable(e));
-	}
-	
-	public void waituntilAlertIsPresent()
-	{
-	   wait = new WebDriverWait(driver,Duration.ofSeconds(10));
-	   wait.until(ExpectedConditions.alertIsPresent());
-	}
-	
-	public void waituntilElementToBeSelectable(WebElement e)
-	{
-		wait = new WebDriverWait(driver,Duration.ofSeconds(10));
-		wait.until(ExpectedConditions.elementToBeSelected(e));
-	}
-	
-	public String GetTitleOfWebPage()
-	{
-	       return driver.getTitle();
-	}
-	
-	public String GetCurrentURLofWebPage()
-	{
-		return driver.getCurrentUrl();
-	}
-	
-	public boolean DisplayedLoGo(WebElement e)
-	{
-		return e.isDisplayed();
-	}
-	
-	public void click(WebElement e)
-	{
-		waitelementtobeClikable(e);
-		e.click();
-	}
-	
-	public String GetText(WebElement e)
-	{
-	   waituntilVisibilityofElement(e);
-	  return e.getText();
-	}
-	
-	public void sendKeys(WebElement e, String Text)
-	{
-		waituntilVisibilityofElement(e);
-		e.sendKeys(Text);
-	}
-	
-	public void NavigateBack()
-	{
-		driver.navigate().back();
-	}
-	
-	public void ClearText(WebElement e)
-	{
-		waituntilVisibilityofElement(e);
-		e.clear();
-	}
-	
-	public void AcceptAlert(WebElement e)
-	{
-		waituntilAlertIsPresent();
-		Alert alert = driver.switchTo().alert();
-		System.out.println("Switched on Alert "+alert.getText());
-		alert.accept();
-	}
-	
-	public void DismissAlert()
-	{
-		waituntilAlertIsPresent();
-		Alert alert = driver.switchTo().alert();
-		System.out.println("Switched on Alert "+alert.getText());
-		alert.dismiss();
-	}
-	
-	public void ScrollPage(int a, int b)
-	{
-		JavascriptExecutor js = (JavascriptExecutor)driver; 
-		js.executeScript("window.scrollBy("+a+","+b+")");
-	}
-	
-	public void ScrollIntoView(WebElement e)
-	{
-		JavascriptExecutor js = (JavascriptExecutor)driver;
-		js.executeScript("Arguments[0].scrollIntoViewIfNeeded", e);
-	}
-	
-	public void BrowserPOPUP()
-	{
-		Set<String> AllWindow = driver.getWindowHandles();
+		driver.get(pro.getProperty("OrangeHRMurl"));
 		
 	}
 	
